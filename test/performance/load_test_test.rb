dc49c35fe818d09b9fc6958155fda1479d780db7
sitrucj <jccollard3@gmail.com>
2015-03-24 01:22:56 -0400

    Cleaned up the user add tests

--- test/performance/load_test_test.rb
+++ test/performance/load_test_test.rb
@@ -8,11 +8,27 @@ class LoadTestTest < ActionDispatch::PerformanceTest
                            	output: 'tmp/performance', formats: [:flat] }
 
     test "Generate 10 Users" do
-  	for i in 0..10
-  		post '/users/new', params: { slug: Faker::Internet.user_name, name:Faker::Name.name, 
-  			email:Faker::Internet.email, password:'password1', password_confirmation:'password1', 
-  			commit:'Sign up' }
-  	end
-  end
+    	for i in 0..10
+    		post '/users/new', params: { slug: Faker::Internet.user_name, name:Faker::Name.name, 
+    			email:Faker::Internet.email, password:'password1', password_confirmation:'password1', 
+    			commit:'Sign up' }
+    	end
+    end
 
-end
+    test "Generate 100 Users" do
+      for i in 0..100
+        post '/users/new', params: { slug: Faker::Internet.user_name, name:Faker::Name.name, 
+          email:Faker::Internet.email, password:'password1', password_confirmation:'password1', 
+          commit:'Sign up' }
+      end
+    end
+
+    test "Generate 1000 Users" do
+      for i in 0..1000
+        post '/users/new', params: { slug: Faker::Internet.user_name, name:Faker::Name.name, 
+          email:Faker::Internet.email, password:'password1', password_confirmation:'password1', 
+          commit:'Sign up' }
+      end
+    end
+
+  end
